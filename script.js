5d7a695034c4e39cc889510d36ae946e22946c3c
Ulises Sosa <ulisesdevsosa@gmail.com>
2022-10-24 09:28:50 -0300

    Rearrenge and modify some console.log

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+let computerSelection;
+let playerSelection;
+let playerScore = 0;
+let computerScore = 0;
+
+
 function getComputerChoice() {
     const random = Math.floor(Math.random() * 3)
     if (random === 0){
@@ -8,56 +14,52 @@ function getComputerChoice() {
         return 'scissors'
     }
 }
-let computerSelection;
-let playerSelection;
-let playerScore = 0;
-let computerScore = 0;
+
 function play(playerSelection, computerSelection) { 
     playerSelection = window.prompt('Make your choice').toLowerCase();
     computerSelection = getComputerChoice();
-    console.log(playerSelection);
-    console.log(computerSelection);
     if (playerSelection === computerSelection){
         console.log(`It's a tie`);
     }else if (playerSelection === 'rock'){
         if(computerSelection === 'paper'){
-            console.log('Computer win');
+            console.log(`Computer win ${computerSelection} beats ${playerSelection}`);
             computerScore++;
-            console.log(`computer score is ${computerScore}`);
         }else{
-            console.log('You Win');
+            console.log(`You win ${playerSelection} beats ${computerSelection}`);
             playerScore++;
-            console.log(`player score is ${playerScore}`);
         }
     }else if (playerSelection === 'paper'){
         if(computerSelection === 'scissors'){
-            console.log('Computer win');
+            console.log(`Computer win ${computerSelection} beats ${playerSelection}`);
             computerScore++;
-            console.log(`computer score is ${computerScore}`);
         }else{
-            console.log('You Win');
+            console.log(`You win ${playerSelection} beats ${computerSelection}`);
             playerScore++;
-            console.log(`player score is ${playerScore}`);
         }
     }else if (playerSelection === 'scissors'){
         if(computerSelection === 'rock'){
-            console.log('Computer win');
+            console.log(`Computer win ${computerSelection} beats ${playerSelection}`);
             computerScore++;
-            console.log(`computer score is ${computerScore}`);
         }else{
-            console.log('You Win');
+            console.log(`You win ${playerSelection} beats ${computerSelection}`);
             playerScore++;
-            console.log(`player score is ${playerScore}`);
         }
     }
 }
 
 function game() {
-    for (let i = 0; i < 15; i++) {
+    for (let i = 0; i < 10; i++) {
         console.log(play(playerSelection, computerSelection));
     }
     console.log(`The final computer score is ${computerScore}`);
     console.log(`The final player score is ${playerScore}`);
+    if (playerScore > computerScore) {
+        console.log(`You win!! you beat the computer ${playerScore} to ${computerScore}`);
+    }else if (computerScore > playerScore){
+        console.log(`You lose!! the computer beat  you ${computerScore} to ${playerScore}`)
+    }else {
+        console.log(`It's a tie ${computerScore} to ${playerScore}`);
+    }
     playerScore = 0;
     computerScore = 0;
 }
