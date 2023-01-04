4d78acefaeb33e4643e1062a2bb45e49d1c6fb62
Ulises Sosa <ulisesdevsosa@gmail.com>
2023-01-04 17:35:27 -0300

    Create the basic UI for the game
    Create 3 buttons, add a class of buttons to each one and make each
    button.textContent equal to one element of the game.
    Create two eventListener, one to make the playerSelection equal to
    the button clicked and other to play a round of the game when a button
    is clicked
    Create a div and append it to the body
    Append the buttons to the div
    Create a two spans named victory and defead to display the result of
    the round
    Append the spans to the div
    Remove the game function to play a 10 round game automaticly

--- script.js
+++ script.js
@@ -15,17 +15,22 @@ function getComputerChoice() {
     }
 }
 
-function play(playerSelection, computerSelection) { 
-    playerSelection = window.prompt('Make your choice').toLowerCase();
+function play() {
     computerSelection = getComputerChoice();
+    victory.textContent = '';
+    defeat.textContent = '';
     if (playerSelection === computerSelection){
         console.log(`It's a tie`);
     }else if (playerSelection === 'rock'){
         if(computerSelection === 'paper'){
             console.log(`Computer win ${computerSelection} beats ${playerSelection}`);
+            defeat.textContent = `Computer win ${computerSelection} beats ${playerSelection}`;
+            div.appendChild(defeat);
             computerScore++;
         }else{
             console.log(`You win ${playerSelection} beats ${computerSelection}`);
+            victory.textContent = `You win ${playerSelection} beats ${computerSelection}`;
+            div.appendChild(victory);
             playerScore++;
         }
     }else if (playerSelection === 'paper'){
@@ -47,7 +52,7 @@ function play(playerSelection, computerSelection) {
     }
 }
 
-function game() {
+/* function game() {
     for (let i = 0; i < 10; i++) {
         console.log(play(playerSelection, computerSelection));
     }
@@ -62,6 +67,43 @@ function game() {
     }
     playerScore = 0;
     computerScore = 0;
+} 
+
+game(); */
+
+const div = document.createElement('div');
+const rock = document.createElement('button');
+const scissors = document.createElement('button');
+const paper = document.createElement('button');
+const victory = document.createElement('span');
+const defeat = document.createElement('span')
+
+
+rock.textContent = 'rock';
+paper.textContent = 'paper';
+scissors.textContent = 'scissors';
+rock.classList.add('buttons');
+paper.classList.add('buttons');
+scissors.classList.add('buttons');
+
+
+div.appendChild(rock);
+div.appendChild(paper);
+div.appendChild(scissors);
+document.body.appendChild(div);
+
+function buttonClicked(e){
+    playerSelection = e.target.textContent;
 }
 
-game();
+
+let buttons = document.querySelectorAll('.buttons');
+buttons.forEach((button) => {
+    button.addEventListener('click', buttonClicked)
+});
+
+buttons.forEach((button) => {
+    button.addEventListener('click', play)
+})
+
+
